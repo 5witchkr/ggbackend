1b6bad21ac224e202cd2363cb54a2bc4b9e2db18
5witchkr <hob4419@gmail.com>
2022-12-08 01:27:56 +0900

    fix(dto): fix dto for change entity

--- src/main/java/com/pickban/ggbackend/recommendpick/dto/RecommendPickDto.java
+++ src/main/java/com/pickban/ggbackend/recommendpick/dto/RecommendPickDto.java
@@ -9,6 +9,6 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class RecommendPickDto {
-    private Long champId;
-    private String champTier;
+    private Long championId;
+    private String championTier;
 }
