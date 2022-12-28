702a8d927454921e92e1143d9abbcc63095b2b00
5witchkr <hob4419@gmail.com>
2022-12-28 23:34:20 +0900

    feat(TierEnum): create TierEnum

--- src/main/java/com/pickban/ggbackend/recommendpick/enummodel/TierEnum.java
+++ src/main/java/com/pickban/ggbackend/recommendpick/enummodel/TierEnum.java
@@ -1,9 +1,19 @@
 package com.pickban.ggbackend.recommendpick.enummodel;
 
-public class TierEnum {
-    //1Tier
-    //2Tier
-    //3Tier
-    //4Tier
-    //5Tier
+public enum TierEnum {
+    TIER1("1Tier"),
+    TIER2("2Tier"),
+    TIER3("3Tier"),
+    TIER4("4Tier"),
+    TIER5("5Tier");
+
+    private final String value;
+
+    TierEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
