a0e3e56becd649bcd3fb651a41a1defc552ef5f1
5witchkr <hob4419@gmail.com>
2022-12-28 23:40:58 +0900

    refactor(ChampionProcessorImpl): apply enummodel

--- src/main/java/com/pickban/ggbackend/recommendpick/domain/processor/ChampionProcessorImpl.java
+++ src/main/java/com/pickban/ggbackend/recommendpick/domain/processor/ChampionProcessorImpl.java
@@ -4,6 +4,8 @@ import com.pickban.ggbackend.recommendpick.domain.entity.Champion;
 import com.pickban.ggbackend.recommendpick.domain.processor.mapper.ChampionMapper;
 import com.pickban.ggbackend.recommendpick.domain.repository.ChampionRepository;
 import com.pickban.ggbackend.recommendpick.dto.ChampionResponseDto;
+import com.pickban.ggbackend.recommendpick.enummodel.ExceptionMsgEnum;
+import com.pickban.ggbackend.recommendpick.enummodel.TierEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -21,20 +23,20 @@ public class ChampionProcessorImpl implements ChampionProcessor{
     @Override
     public List<ChampionResponseDto> getCounter(String emLineChamp) {
         Champion champion = championRepository.findByChampionId(Long.valueOf(emLineChamp))
-                .orElseThrow(() -> new NullPointerException("Not Found Champion!!"));
+                .orElseThrow(() -> new NullPointerException(ExceptionMsgEnum.NOT_FOUND.getValue()));
                 
         List<Long> counterList = champion.getCounters();
         List<ChampionResponseDto> championResponseDtoList = new ArrayList<>();
         counterList.forEach(champId -> championResponseDtoList.add(
                 championMapper.championToChampionResponseDto(
                         championRepository.findByChampionId(champId)
-                                .orElseThrow(() -> new NullPointerException("Not Found Champion!!!")))));
+                                .orElseThrow(() -> new NullPointerException(ExceptionMsgEnum.NOT_FOUND.getValue())))));
         return championResponseDtoList;
     }
 
     @Override
     public List<ChampionResponseDto> getTopTier(String line) {
-        List<Champion> championList = championRepository.findByPositionAndChampionTier(line, "1Tier");
+        List<Champion> championList = championRepository.findByPositionAndChampionTier(line, TierEnum.TIER1.getValue());
         return championMapper.championListToChampionResponseDtoList(championList);
     }
 
