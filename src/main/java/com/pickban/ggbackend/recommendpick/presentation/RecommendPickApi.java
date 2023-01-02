4239f96992cc006678c394b09506bc3555c5cee3
5witchkr <hob4419@gmail.com>
2023-01-02 15:50:01 +0900

    refactor(RecommendPickApi): add ResponseEntity generic

--- src/main/java/com/pickban/ggbackend/recommendpick/presentation/RecommendPickApi.java
+++ src/main/java/com/pickban/ggbackend/recommendpick/presentation/RecommendPickApi.java
@@ -4,10 +4,14 @@ package com.pickban.ggbackend.recommendpick.presentation;
 import com.pickban.ggbackend.recommendpick.application.RecommendPickFacade;
 import com.pickban.ggbackend.recommendpick.constantmodel.ChampValueConst;
 import com.pickban.ggbackend.recommendpick.domain.MockDataSave;
+import com.pickban.ggbackend.recommendpick.dto.ProgamerPickDto;
+import com.pickban.ggbackend.recommendpick.dto.RecommendPickDto;
 import com.pickban.ggbackend.recommendpick.dto.RecommendRequestDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/recommend")
@@ -23,8 +27,8 @@ public class RecommendPickApi {
     }
 
     @GetMapping("")
-    public ResponseEntity getPick(@RequestParam String team, String line, String bans, String emBans, String picks, String emPicks) {
-        return new ResponseEntity(recommendPickFacade.getRecommend(team, line,
+    public ResponseEntity<List<RecommendPickDto>> getPick(@RequestParam String team, String line, String bans, String emBans, String picks, String emPicks) {
+        return new ResponseEntity<>(recommendPickFacade.getRecommend(team, line,
                 RecommendRequestDto.builder()
                         .bans(checkParam(bans))
                         .emBans(checkParam(emBans))
@@ -34,8 +38,8 @@ public class RecommendPickApi {
     }
 
     @GetMapping("/progamer")
-    public ResponseEntity getPickProgamer(@RequestParam String team, String line, String bans, String emBans, String picks, String emPicks) {
-        return new ResponseEntity(recommendPickFacade.getRecommendProgamer(team, line,
+    public ResponseEntity<List<ProgamerPickDto>> getPickProgamer(@RequestParam String team, String line, String bans, String emBans, String picks, String emPicks) {
+        return new ResponseEntity<>(recommendPickFacade.getRecommendProgamer(team, line,
                 RecommendRequestDto.builder()
                         .bans(checkParam(bans))
                         .emBans(checkParam(emBans))
@@ -45,13 +49,13 @@ public class RecommendPickApi {
     }
 
     @PostMapping("/dev")
-    public ResponseEntity saveChampions() {
+    public ResponseEntity<HttpStatus> saveChampions() {
         mockDataSave.saveTestData();
-        return new ResponseEntity(HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
     @GetMapping("/dev/{champId}")
-    public ResponseEntity findChampions(@PathVariable Long champId) {
-        return new ResponseEntity(mockDataSave.findTestData(champId), HttpStatus.OK);
+    public ResponseEntity<Object> findChampions(@PathVariable Long champId) {
+        return new ResponseEntity<>(mockDataSave.findTestData(champId), HttpStatus.OK);
     }
 
     private String checkParam(String championId) {
