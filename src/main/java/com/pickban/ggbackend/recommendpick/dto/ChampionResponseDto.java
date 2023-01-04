29fe1edfdcec4871fe57d4d317c5b06d92b23c41
5witchkr <hob4419@gmail.com>
2023-01-04 17:18:35 +0900

    refactor(ChampionResponseDto): remove unused lombok dependency

--- src/main/java/com/pickban/ggbackend/recommendpick/dto/ChampionResponseDto.java
+++ src/main/java/com/pickban/ggbackend/recommendpick/dto/ChampionResponseDto.java
@@ -1,37 +1,27 @@
 package com.pickban.ggbackend.recommendpick.dto;
 
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class ChampionResponseDto {
     private Long championId;
     private String championTier;
     private Long opScore;
 
-    public ChampionResponseDto(Long championId) {
+    public ChampionResponseDto() {
+    }
+
+    @Builder
+    public ChampionResponseDto(Long championId,
+                                    String championTier,
+                                    Long opScore) {
         this.championId = championId;
+        this.championTier = championTier;
+        this.opScore = opScore;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o instanceof ChampionResponseDto) {
-            return championId.equals(((ChampionResponseDto) o).championId);
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return championId.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return String.valueOf(championId);
-    }
 }
